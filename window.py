5d7b2985379948f2684d57a3bd9c96562586838a
BraulioISG <soloriobraulio03@gmail.com>
2022-01-20 16:24:40 -0600

    window & settings

---
+++ window.py
@@ -0,0 +1,21 @@
+import pygame
+from config import settings as sett
+
+print(sett.get("Fullscreen"))
+
+#getting the dimensions 
+width = sett.get("Screen_width")
+height = sett.get("Screen_height")
+
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.flip()
+
+loop = True
+
+#main loop
+while loop:
+    for event in pygame.event.get():
+        #exit handleing
+        if event.type == pygame.QUIT:
+            loop = False
